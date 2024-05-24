4303e6354c3c1b4934837ba125e6c8547999904f
Priyanshu Lakra <priyanshulakra0061@gmail.com>
2024-05-24 17:49:15 +0530

    changed the appbar .. the one who is signed in .. the first letter of his email will be shown on right top corener

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -8,73 +8,105 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 // import withAccelerate from 'prisma-with-accelerate';
 
-import { sign , verify } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 import { signinInput, signupInput } from "@priyanshulakra/medium-common";
 
 
-export  const userRouter = new Hono<{
+export const userRouter = new Hono<{
 
-    Bindings:{
-        DATABASE_URL:string;
-        JWT_SECRET:string;
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
     }
 }>();
 
 userRouter.post('/signup', async (c) => {
-    
+
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
-        }).$extends(withAccelerate())
-   
+    }).$extends(withAccelerate())
+
     const body = await c.req.json();
-    const {success} = signupInput.safeParse(body);
-    if(!success){
+    const { success } = signupInput.safeParse(body);
+    if (!success) {
         c.status(411);
         return c.json({
-            message:"inputs are not correct"
+            message: "inputs are not correct"
         })
     }
     // the user has specific id .. which is used to create jwt token for it when signed up
     const user = await prisma.user.create({
-      data:{
-        email:body.email,
-        password:body.password
-      }
+        data: {
+            email: body.email,
+            password: body.password
+        }
     })
-    const token = await sign({ id: user.id },"Lakra@123");
-        return c.json({ jwt:token });
+    const token = await sign({ id: user.id }, "Lakra@123");
+    return c.json({ jwt: token });
 })
 
 
 userRouter.post('/signin', async (c) => {
 
-const prisma = new PrismaClient({
-datasourceUrl:c.env.DATABASE_URL,
-}).$extends(withAccelerate())
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
 
-const  body = await c.req.json();
-const {success} = signinInput.safeParse(body);
-    if(!success){
+    const body = await c.req.json();
+    const { success } = signinInput.safeParse(body);
+    if (!success) {
         c.status(411);
         return c.json({
-            message:"inputs are not correct"
+            message: "inputs are not correct"
         })
     }
-const user = await prisma.user.findUnique({
-where:{
-  email:body.email,
-  password:body.password
-}
+    const user = await prisma.user.findUnique({
+        where: {
+            email: body.email,
+            password: body.password
+        }
+    })
+
+
+    if (!user) {
+        c.status(403);
+        return c.json({ error: "user not found" });
+    }
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+
+
 })
 
 
-if(!user){
-c.status(403);
-    return c.json({ error: "user not found" });
-}
+userRouter.get('/:id', async (c) => {
 
-const jwt = await sign({id:user.id} , c.env.JWT_SECRET);
-return c.json({jwt});
+    const id = c.req.param("id");
+    console.log(id);
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+    try {
+
+        const details = await prisma.user.findFirst({
+            where: {
+                id: Number(id)
+            },
+            select: {
+                email:true
+            }
+        })
+        
+        return c.json({
+            details
+        })
+    } catch (e) {
+        c.status(411);
+        return c.json({
+            message: "Error while fetching the "
+        })
+    }
 
 
 })
--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,8 +1,58 @@
 import { Link } from "react-router-dom"
 import { Avatar } from "./BlogCard"
 
+import { JwtPayload, jwtDecode } from "jwt-decode";
 
-export const Appbar = () => {
+import { BACKEND_URL } from "../config";
+import { useEffect, useState } from "react";
+
+interface UserPayload extends JwtPayload {
+    id: string;
+  }
+export const getUserInfoFromToken = (): UserPayload | null =>{
+
+  const token = localStorage.getItem('token');
+  if (token) {
+    try {
+      const decodedToken = jwtDecode<UserPayload>(token)
+      return decodedToken; // returns the entire payload object
+    } catch (error) {
+      console.error('Invalid token:', error);
+      return null;
+    }
+  }
+  return null;
+};
+
+export const  Appbar = () => {
+
+    // getting the emial of the sigined user from the jwt token which gets stored in the local storage as someone sign in to website
+    const [Email , SetEmail] = useState('');
+    useEffect(()=>{
+      const fetchEmail = async () => {
+        try {
+          const userInfo = getUserInfoFromToken();
+          const userId = userInfo ? userInfo.id : null;
+          if (userId) {
+            const response = await fetch(`${BACKEND_URL}/api/v1/user/${userId}`);
+            if (response.ok) {
+              const data = await response.json();
+              SetEmail(data.details.email);
+            } else {
+              console.error('Failed to fetch email:', response.statusText);
+            }
+          }
+        } catch (error) {
+          console.error('Error fetching email:', error);
+        }
+      };
+      fetchEmail();
+      
+
+    } , [])
+
+    
+    
     return <div className="border-b flex justify-between px-10 py-4">
         <Link to={'/blogs'} className="flex flex-col justify-center cursor-pointer">
             medium
@@ -14,7 +64,7 @@ export const Appbar = () => {
                 <button type="button" className=" mr-4 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">New</button>
 
             </Link>
-            <Avatar size={8} name="priyanshu Lakra"></Avatar>
+            <Avatar size={8} name={Email}></Avatar>
         </div>
 
     </div>
